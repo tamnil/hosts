ab33080e2a8297f7abdb96660dd56bccfcb44090
Tamnil Saito Junior <tamnil@gmail.com>
2019-05-04 17:50:36 -0300

    sync

--- decrypt.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# tar -cz personal | gpg -c -o myarchive.tar.gz.gpg
-gpg -d personal.gpg | tar xz
--- encrypt.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-# gpg  --batch -c blacklist
-#-c whitelist -c myhosts 
-# gpg --batch -c whitelist --batch
-# gpg -c myhosts --batch
-tar -cz personal | gpg -c -o personal.gpg
-
-
